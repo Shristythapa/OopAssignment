9965b9459660fe2cd9d23d22b6aea0181b5debc6
Shristythapa <thapashristy110@gmail.com>
2022-04-23 14:37:58 +0545

    question no17 added

--- conditional.java
+++ conditional.java
@@ -380,6 +380,44 @@ class Question16 {
         }
     }
 }
+class Question17{
+    public static void main(String[] args) {
+        double a,b,c;
+        double root1,root2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of A:: ");
+        a=scanner.nextDouble();
+        System.out.print("Enter the value of b:: ");
+        b=scanner.nextDouble();
+        System.out.print("Enter the value of c:: ");
+        c=scanner.nextDouble();
+
+        double determinant=b*b-4*a*c;
+
+        if(determinant>0){
+            //two real and distinct roots
+            root1=(-b+Math.sqrt(determinant))/(2*a);
+            root2=(-b-Math.sqrt(determinant))/(2*a);
+            System.out.format("root1=%2f and root2=%2f",root1,root2);
+        }
+        else if(determinant==0){
+            //real and equal roots
+            root1=root2=-b/(2*a);
+            System.out.format("root1=root2=%2f",root1);
+        }
+        else{
+            //roots are complex and distinct
+            double real = -b/(2*a);
+            double imaginary=Math.sqrt(-determinant)/(2*a);
+            System.out.format("root1 = %.2f+%.2fi", real, imaginary);
+            System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
+
+
+            System.out.format("root1=%2f+%2f",real,imaginary);
+            System.out.format("\nroot2=%2f")
+        }
+    }
+}
 class Question18 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
